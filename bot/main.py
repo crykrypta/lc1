433555887ad99c7183283d604a48172543140ce8
crykrypta <crykrypta@gmail.com>
2024-11-23 14:06:23 +0300

    update bot/main.py

---
+++ bot/main.py
@@ -0,0 +1,29 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from common.logs import LogConfig
+from common.config import load_config
+
+
+# Настройка логирования
+logger = LogConfig.setup_logging()
+# Загрузка конфигурации
+config = load_config()
+
+# Создание бота и диспетчера
+bot = Bot(token=config.bot.api_key)
+dp = Dispatcher()
+
+
+# Асинхронная функция для запуска бота
+async def main():
+    await dp.start_polling(bot)
+
+
+# Запуск бота
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+        logger.info('Bot started')
+    except KeyboardInterrupt:
+        logger.info('Bot stopped')
