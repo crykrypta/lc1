123cae9b323fab0aabb6bd86dc20a6dcc0c6753c
crykrypta <crykrypta@gmail.com>
2024-11-23 13:58:00 +0300

    first commin

---
+++ common/config.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class Bot:
+    api_key: str
+
+
+@dataclass
+class OpenAI:
+    api_key: str
+
+
+@dataclass
+class Config:
+    bot: Bot
+    openai: OpenAI
+
+
+def load_config() -> Config:
+    env = Env()
+    env.read_env()
+    return Config(
+        bot=Bot(api_key=env.str("BOT_TOKEN")),
+        openai=OpenAI(api_key=env.str("OPENAI_API_KEY"))
+    )
